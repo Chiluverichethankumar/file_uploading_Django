f9801b18b673e4480a05f9533c81edf725f96364
Chiluverichethankumar <132481430+Chiluverichethankumar@users.noreply.github.com>
2025-05-31 13:15:10 +0530

    Delete django_backend directory

--- django_backend/CRU/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
-from .models import UploadedFile # Import your UploadedFile model
-
-# Register your model with the admin site
-admin.site.register(UploadedFile)
--- django_backend/CRU/serializers.py
+++
@@ -1,9 +0,0 @@
-# app_name/serializers.py
-from rest_framework import serializers
-from .models import UploadedFile
-
-class UploadedFileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UploadedFile
-        fields = '__all__' # Or specify individual fields like ['id', 'file', 'description', 'uploaded_at']
-
