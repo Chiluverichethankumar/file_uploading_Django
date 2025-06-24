e14bd0e44aa316ad6303b1d08c14f8b375659c1c
Chiluverichethankumar <chiluverichethankumar@gmail.com>
2025-06-24 13:24:01 +0530

    Your commit message

--- CRU/models.py
+++ CRU/models.py
@@ -1,17 +1,9 @@
-# from django.db import models
-
-
-# class CallRecording(models.Model):
-#     filename = models.CharField(max_length=255)
-#     uploaded_at = models.DateTimeField(auto_now_add=True)
-
-
 from django.db import models
 
 class CallRecording(models.Model):
     file = models.FileField(upload_to='recordings/')
-    note = models.TextField(blank=True, null=True)
-    uploaded_at = models.DateTimeField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    note = models.TextField(blank=True)
 
     def __str__(self):
-        return str(self.file.name)
+        return self.file.name
