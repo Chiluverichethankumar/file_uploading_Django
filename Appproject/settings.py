94436f376625801274512fac1fa666885695dc5a
ChiluverchethanKumar <chiluverichethankumar@gmail.com>
2025-07-29 00:37:07 +0530

    Add render domain to ALLOWED_HOSTS

--- Appproject/settings.py
+++ Appproject/settings.py
@@ -178,7 +178,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-for-dev'
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 # Allowed hosts
-ALLOWED_HOSTS = ['file-uploading-django.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'file-uploading-django.onrender.com,localhost,127.0.0.1').split(',')
+
 
 # Application definition
 
